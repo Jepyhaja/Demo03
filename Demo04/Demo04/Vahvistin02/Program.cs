0cb4826519d766d2a7f035ca6e4cc60f6ed379d8
H9175 <H9175@student.jamk.fi>
2017-02-02 15:34:18 +0200

    vahvari done

---
+++ Demo04/Demo04/Vahvistin02/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vahvistin02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vahvistin vahvari = new Vahvistin();
+
+
+
+            int luku;
+
+            do
+            {
+
+                Console.WriteLine("vahvistin volume: " + vahvari.Vol);
+                Console.WriteLine("laita volume 0-100");
+                string kerros = Console.ReadLine();
+                vahvari.Vol = int.Parse(kerros);
+                luku = int.Parse(kerros);
+
+
+
+            } while (luku != 0);
+        }
+    }
+}
---
+++ Demo04/Demo04/Vahvistin02/Vahvistin.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vahvistin02
+{
+    class Vahvistin
+    {
+        private int MaxVol = 100;
+        private int MinVOl = 0;
+
+        private int vol;
+        public int Vol
+        {
+            get
+            {
+                return vol;
+            }
+
+            set
+            {
+                if(value <= MaxVol && value >= MinVOl)
+                {
+                    vol = value;
+                }
+                else if(value < MinVOl)
+                {
+                    Console.WriteLine("You cannot set volume lower than " + MinVOl);
+                    Vol = vol;
+                }
+
+                else if(value < MaxVol)
+                {
+                    Console.WriteLine("You cannot set volume higher than " + MaxVol);
+                    Vol = vol;
+                }
+            }
+        }
+
+
+
+
+    }
+}
