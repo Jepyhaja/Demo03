be590aceb5e283a6485bd153e2fd1b611d2b9aa9
H9175 <H9175@student.jamk.fi>
2017-02-09 15:02:49 +0200

    Demo05

---
+++ Demo05/Esimerkki/Demo05/Persons.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo05
+{
+    /// <summary>
+    /// This class holds person objects in a collection.
+    /// </summary>
+    class Persons
+    {
+        /// <summary>
+        /// Collection to store person objects.
+        /// </summary>
+        private List<Person> persons;
+
+        /// <summary>
+        /// Default constructor initializes a collection
+        /// </summary>
+        public Persons()
+        {
+            persons = new List<Person>();
+        }
+        /// <summary>
+        /// Add person to collection.
+        /// </summary>
+        /// <param name="person">Person to add</param>
+        public void AddPersons(Person person)
+        {
+            persons.Add(person);
+        }
+        /// <summary>
+        /// returns a person to caller application
+        /// </summary>
+        /// <param name="index">iondex in collection</param>
+        /// <returns>person object or null</returns>
+        public Person GetPerson(int index)
+        {
+            if (index < persons.Count) return persons.ElementAt(index);
+            else return null;
+        }
+        /// <summary>
+        /// Searches persons ssn from collection
+        /// </summary>
+        /// <param name="socialSecurityNumber">ssn to be searched through collection</param>
+        /// <returns>returns person with matching ssn</returns>
+        public Person FindPerson(string socialSecurityNumber)
+        {
+            foreach(Person person in persons)
+            {
+                if (socialSecurityNumber.Equals(person.SocialSecurityNumber))
+                {
+                    return person;
+                }
+            }
+            //no person matches
+            return null;
+        }
+        public void PrintCollection()
+        {
+            foreach(Person person in persons)
+            {
+                Console.WriteLine(person.ToString());
+            }
+        }
+        
+
+
+    }
+}
---
+++ Demo05/Esimerkki/Demo05/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create person objects
+            Person person1 = new Person
+            {
+                Firstname = "Jari", Lastname = "Kernel", SocialSecurityNumber = "1234-13"
+            };
+
+            Person person2 = new Person
+            {
+                Firstname = "Kirsi",
+                Lastname = "Kernel",
+                SocialSecurityNumber = "2345-19"
+            };
+
+            Person person3 = new Person
+            {
+                Firstname = "Lari",
+                Lastname = "Onntts",
+                SocialSecurityNumber = "3456-22"
+            };
+
+            Person person4 = new Person
+            {
+                Firstname = "Jani",
+                Lastname = "Du'pont",
+                SocialSecurityNumber = "4567-15"
+            };
+
+            //create Persons object
+            Persons myFriends = new Persons();
+
+
+            //add person objects to persons collection
+
+            myFriends.AddPersons(person1);
+            myFriends.AddPersons(person2);
+            myFriends.AddPersons(person3);
+            myFriends.AddPersons(person4);
+
+            //get one person form persons collection
+            Person person5 = myFriends.GetPerson(10);
+                if(person5 != null)
+            {
+                Console.WriteLine(person5.ToString());
+            }
+            else
+            {
+                Console.WriteLine("Person not found or does not exist in that index!");
+            }
+
+            //print collection
+            myFriends.PrintCollection();
+
+            //find person
+            string ssn = "2345-19";
+            Console.WriteLine("Find SSN : " + ssn);
+            Person person6 = myFriends.FindPerson(ssn);
+            if (person6 != null)
+            {
+                Console.WriteLine(person6.ToString());
+            }else
+            {
+                Console.WriteLine("can't find person with that SSN!");
+            }
+
+
+
+        }
+    }
+}
