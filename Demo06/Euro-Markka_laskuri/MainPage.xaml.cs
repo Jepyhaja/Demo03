89fa3416a303b6dbff6f0ece78cdf47eaf033524
Jepyhaja <jepyhaja@gmail.com>
2017-02-22 20:34:30 +0200

    Wintoosa tuuls

---
+++ Demo06/Euro-Markka_laskuri/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Euro_Markka_laskuri
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+        }
+        //must set to flip flops properly
+        double value;
+        string feed;
+
+        private void Euroiksi_button_Click(object sender, RoutedEventArgs e)
+        {
+            //Easymode
+            value = double.Parse(FeedBox.Text);
+            value = value / 5.94573;
+            PrintBox.Text = value.ToString("0.00");
+        }
+
+        private void Markoiksi_button_Click(object sender, RoutedEventArgs e)
+        {
+            //Trickcoding flippin flops
+            feed = FeedBox.Text.ToString();
+            value = double.Parse(feed);
+            value = value * 5.94573;
+            PrintBox.Text = value.ToString("0.00");
+
+        }
+    }
+}
---
+++ Demo06/Ikkuna_pinta-ala/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Ikkuna_pinta_ala
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        double Kwidth;
+        double Garea;
+        double Warea;
+        double Kcirc;
+
+
+        private void Laske_Click(object sender, RoutedEventArgs e)
+        {
+       
+            Width = double.Parse(Leveys.Text);
+            Height = double.Parse(Korkeus.Text);
+            Kwidth = double.Parse(Karmipuu.Text);
+
+
+            Garea = (Width - 2 * Kwidth) * (Height - 2 * Kwidth) / 100;
+            LasinAla_out.Text = Garea.ToString() + " cm^2";
+
+            Warea = Width * Height /100;
+            IkkunanAla_out.Text = Warea.ToString() + "cm^2";
+
+            Kcirc = (Width + Width + Height + Height) / 10;
+            Karminpiiri_out.Text = Kcirc.ToString() + "cm";
+
+        }
+    }
+}
